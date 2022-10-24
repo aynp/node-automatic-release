6d95802b204d4fa2b2bad0a44ce8114322ec5aa6
Aryan <aryan.pathania2003@gmail.com>
2022-10-24 19:05:15 +0530

    ✨ Impliment basic functionality

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 const token = core.getInput('token');
+const draft = core.getInput('draft');
+const generate_release_notes = core.getInput('generate_release_notes');
 
 const octokit = github.getOctokit(token);
 const context = github.context;
@@ -14,15 +16,30 @@ async function main() {
     per_page: 1,
   });
 
-  console.log(data);
+  let previousVersion = '';
 
-  if (data.length === 0) console.log('Initial Release');
+  if (data.length !== 0) {
+    previousVersion = data[0].tag_name;
+  }
 
-  octokit.rest.repos.createRelease({
-    ...context.repo,
-    tag_name: newVersion,
-    draft: true,
-  });
+  if (previousVersion === newVersion) {
+    core.setFailed(`Version did not change`);
+    return;
+  }
+
+  // Publishing release
+  try {
+    octokit.rest.repos.createRelease({
+      ...context.repo,
+      tag_name: newVersion,
+      name: newVersion,
+      previous_tag_name: previousVersion,
+      generate_release_notes: generate_release_notes,
+      draft: draft,
+    });
+  } catch (err) {
+    core.setFailed(`Failed to create a release. ${err}`);
+  }
 }
 
 main();
